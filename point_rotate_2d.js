5ad1a432866503880ed53b4ac275dbe6fe420319
Kelly Milligan <kelly@milligan.net.nz>
2016-06-26 14:09:04 +0200

    Update and rename rotate_2d.js to point_rotate_2d.js

--- point_rotate_2d.js
+++ point_rotate_2d.js
@@ -3,7 +3,7 @@ define( function () {
     'use strict';
 
     /*
-        Simple mathematical 2D rotation in cartesian coordinate space
+        Simple mathematical 2D point rotation in cartesian coordinate space
         ---
         Source: http://stackoverflow.com/a/17411276/6512315
 
