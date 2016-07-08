788366cbdd2f6f2ad80d99166803bbed9a9ba882
Kelly Milligan <kelly@milligan.net.nz>
2016-07-08 16:26:26 +0200

    Tidying rotate 2d util

--- point_rotate_2d.js
+++ point_rotate_2d.js
@@ -7,10 +7,10 @@ define( function () {
         ---
         Source: http://stackoverflow.com/a/17411276/6512315
 
-        cX            int         X origin coordinate to rotate around
-        cY            int         Y origin coordinate to rotate around
-        x             int         X coordinate to be rotated
-        y             int         Y coordinate to be rotated
+        cX            num         X origin coordinate to rotate around
+        cY            num         Y origin coordinate to rotate around
+        x             num         X coordinate to be rotated
+        y             num         Y coordinate to be rotated
         a             rad/deg     Angle to rotate by, positive for clockwise, negative for A.C.
 
         inDegrees     bool        optional - Flag whether angle is passed in as degrees
@@ -20,7 +20,7 @@ define( function () {
     return function (cX, cY, x, y, a, inDegrees) {
 
         // Convert to radians if flagged as degrees
-        a = inDegrees ? a * ( Math.PI / 180 ) : a;
+        a = inDegrees === true ? a * ( Math.PI / 180 ) : a;
 
         var nX = ( Math.cos( a ) * ( x - cX ) ) + ( Math.sin( a ) * ( y - cY ) ) + cX;
         var nY = ( Math.cos( a ) * ( y - cY ) ) - ( Math.sin( a ) * ( x - cX ) ) + cY;
