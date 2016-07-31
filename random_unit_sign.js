f212d856be446d6cbb6659e028fb905316c6e397
Kelly Milligan <kelly@milligan.net.nz>
2016-07-31 15:22:33 +0200

    Add random unit sign util

---
+++ random_unit_sign.js
@@ -0,0 +1,15 @@
+define( function () {
+
+    'use strict';
+
+    /*
+        Return a random unit signed value ( -1 or +1 )
+
+    */
+
+    return function randomUnitSign() {
+
+        return ( Math.random() > 0.5 ) ? 1 : -1;
+    };
+
+});
