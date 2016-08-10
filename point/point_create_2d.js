88f61fd09446cd4ef2c01921d6a315b4c1460b9e
Kelly Milligan <kelly@milligan.net.nz>
2016-08-10 11:32:59 +0200

    Update point create utils to accept initial origin coordinates

--- point/point_create_2d.js
+++ point/point_create_2d.js
@@ -3,20 +3,27 @@ define( function () {
     'use strict';
 
     /*
-        Return a new 2D point object
+        Return a new 2D point object. Optionally supply default origin coordinates
         ---
 
+        oX           num      optional - Initial origin X coordinate
+        oY           num      optional - Initial origin Y coordinate
+
+        --
         Returns      obj      object with point origin and current X and Y coordinates
 
     */
 
-    return function () {
+    return function (oX, oY) {
+
+        oX = oX || 0;
+        oY = oY || 0;
 
         return {
 
             // Origin coordinates
-            oX: 0,
-            oY: 0,
+            oX: oX,
+            oY: oY,
 
             // Current coordinates
             x: 0,
--- point/point_create_3d.js
+++ point/point_create_3d.js
@@ -3,21 +3,30 @@ define( function () {
     'use strict';
 
     /*
-        Return a new 3D point object
+        Return a new 3D point object. Optionally supply default origin coordinates
         ---
 
+        oX           num      optional - Initial origin X coordinate
+        oY           num      optional - Initial origin Y coordinate
+        oZ           num      optional - Initial origin Z coordinate
+
+        --
         Returns      obj      object with point origin and current X, Y, and Z coordinates
 
     */
 
-    return function () {
+    return function (oX, oY, oZ) {
+
+        oX = oX || 0;
+        oY = oY || 0;
+        oZ = oZ || 0;
 
         return {
 
             // Origin coordinates
-            oX: 0,
-            oY: 0,
-            oZ: 0,
+            oX: oX,
+            oY: oY,
+            oZ: oZ,
 
             // Current coordinates
             x: 0,
