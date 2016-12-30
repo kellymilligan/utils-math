68627b12305930429dabf41a3bade7d526c840eb
Kelly Milligan <kelly@milligan.net.nz>
2016-12-30 13:14:44 +1300

    Update Point on Circle util to ES6

--- point/point_on_circle.js
+++ point/point_on_circle.js
@@ -1,30 +1,30 @@
-define( function () {
+/*
+    Get the X and Y position of a point on a circle at supplied angle
+    ---
+    http://stackoverflow.com/a/839931/6512315
 
-    'use strict';
+    cX            num         X origin coordinate at centre of circle
+    cY            num         Y origin coordinate at centre of circle
+    cR            num         Radius of the circle from it's origin
+    a             rad/deg     Angle to rotate by, positive for clockwise, negative for A.C.
 
-    /*
-        Get the X and Y position of a point on a circle at supplied angle
-        ---
-        http://stackoverflow.com/a/839931/6512315
+    inDegrees     bool        optional - Flag whether angle is passed in as degrees
 
-        cX            num         X origin coordinate at centre of circle
-        cY            num         Y origin coordinate at centre of circle
-        cR            num         Radius of the circle from it's origin
-        a             rad/deg     Angle to rotate by, positive for clockwise, negative for A.C.
+*/
 
-        inDegrees     bool        optional - Flag whether angle is passed in as degrees
+export default function (
 
-    */
+    cX, cY, cR, a,
 
-    return function (cX, cY, cR, a, inDegrees) {
+    inDegrees = false
 
-        // Convert to radians if flagged as degrees
-        if ( inDegrees === true ) { a = ( Math.PI / 180 ) * a; }
+) {
 
-        var x = cX + cR * Math.cos( a );
-        var y = cY + cR * Math.sin( a );
+    // Convert to radians if flagged as degrees
+    if ( inDegrees === true ) { a = ( Math.PI / 180 ) * a; }
 
-        return { x: x, y: y };
-    };
+    var x = cX + cR * Math.cos( a );
+    var y = cY + cR * Math.sin( a );
 
-});
+    return { x: x, y: y };
+}
