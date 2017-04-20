67224a843cf1d547464924a17b7e22f82450c54d
Kelly Milligan <kelly@milligan.net.nz>
2017-04-20 16:05:27 +0100

    Add math constants export util

---
+++ constants.js
@@ -0,0 +1,12 @@
+/*
+    Export list for commonly used math constants
+*/
+
+// Rotation
+export const PI = Math.PI;
+export const TWO_PI = Math.PI * 2;
+export const HALF_PI = Math.PI * 0.5;
+
+// Conversion
+export const DEG_TO_RAD = Math.PI / 180;
+export const RAD_TO_DEG = 180 / Math.PI;
