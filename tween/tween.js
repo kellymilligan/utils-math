4d60033884f4562ddd73545f3c4874a8bf1449cc
Kelly Milligan <kelly@milligan.nz>
2019-05-03 10:53:08 -0400

    2019 Style adjustments

--- tween/tween.js
+++ tween/tween.js
@@ -1,133 +1,129 @@
 /*
-    Minimal tween class for interpolating a normalised progress value over a duration.
-    Designed to work directly with single-input Penner easing functions.
-    ---
+  Minimal tween class for interpolating a normalised progress value over a duration.
+  Designed to work directly with single-input Penner easing functions.
+  ---
+  duration            Number      Duration of the tween in milliseconds
+  easing              String      Name of the easing function to apply
+  onTickCallback      Function    Callback function to be fired each tick
+  onCompleteCallback  Function    Callback function to be fired when the tween completes
 
-    duration               Number        Duration of the tween in milliseconds
-    easing                 String        Name of the easing function to apply
-    onTickCallback         Function      Callback function to be fired each tick
-    onCompleteCallback     Function      Callback function to be fired when the tween completes
+  Notes:
+  - Tick callback is passed both the `value` (eased) and `progress` (linear) values as arguments
+  - Tween must be started by calling the `start()` function
+  - Tween can be stopped early by calling the `kill` function
+  - For garbage collection, remove reference to the instance in the onCompleteCallback handler
 
-    Notes:
+  Example usage:
 
-    - Tick callback is passed both the 'value' (eased) and 'progress' (linear) values as arguments
-    - Tween must be started by calling the 'start' function
-    - Tween can be stopped early by calling the 'kill' function
-    - For garbage collection, remove reference to the class instance in the onCompleteCallback handler
-
-    Example usage:
-
-    let tween = new Tween(
-        1000,
-        'easeInOutCubic',
-        (value, progress) => { console.log( value, progress ); },
-        () => { tween = null; }
-    );
-
-    tween.start();
-
+  const tween = new Tween(
+    1000,
+    'easeInOutCubic',
+    ( value, progress ) => { console.log( value, progress ) },
+    () => { tween = null }
+  )
+  tween.start()
 */
 
-import * as EasingFunctions from '../easing.js';
+import * as EasingFunctions from '../easing.js'
 
-const requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+const requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
 
 export default class {
 
-    constructor(
+  constructor(
 
-        duration = 1000,
-        easing = 'easeInOutSine',
-        onTickCallback = function () {},
-        onCompleteCallback = function () {}
+    duration = 1000,
+    easing = 'easeInOutSine',
+    onTickCallback = function () {},
+    onCompleteCallback = function () {}
 
-    ) {
+  ) {
 
-        this.duration = duration;
-        this.easing = easing;
-        this._onTick = onTickCallback;
-        this._onComplete = onCompleteCallback;
+    this.duration = duration
+    this.easing = easing
+    this._onTick = onTickCallback
+    this._onComplete = onCompleteCallback
 
-        this.progress = 0;
-        this.value = 0;
-        this.running = false;
+    this.progress = 0
+    this.value = 0
+    this.running = false
 
-        this.startTimer = null;
+    this.startTimer = null
+  }
+
+  _tick () {
+
+    // Check if tween has been killed
+    if ( !this.running ) {
+
+      this._destroy()
+      return
     }
 
-    _tick() {
+    // Check for completeness
+    if ( this.progress >= 1 ) {
 
-        // Check if tween has been killed
-        if ( !this.running ) {
+      this.progress = 1
+      this.value = 1
 
-            this._destroy();
-            return;
-        }
+      this._onTick( this.value, this.progress )
+      this._complete()
 
-        // Check for completeness
-        if ( this.progress >= 1 ) {
-
-            this.progress = 1;
-            this.value = 1;
-
-            this._onTick( this.value, this.progress );
-            this._complete();
-
-            return;
-        }
-
-        // Continue with next tick
-        this.currentTime = Date.now();
-
-        this.progress = ( this.currentTime - this.startTime ) / this.duration;
-        this.value = EasingFunctions[ this.easing ]( this.progress );
-
-        this._onTick( Math.min( this.value, 1 ), Math.min( this.progress, 1 ) );
-
-        this.animationFrame = requestAnimationFrame( () => this._tick() );
+      return
     }
 
-    _start() {
+    // Continue with next tick
+    this.currentTime = Date.now()
 
-        this.running = true;
+    this.progress = ( this.currentTime - this.startTime ) / this.duration
+    this.value = EasingFunctions[ this.easing ]( this.progress )
 
-        this.startTime = Date.now();
-        this.currentTime = this.startTime;
+    this._onTick( Math.min( this.value, 1 ), Math.min( this.progress, 1 ) )
 
-        this.animationFrame = requestAnimationFrame( () => this._tick() );
-    }
+    this.animationFrame = requestAnimationFrame( () => this._tick() )
+  }
 
-    _kill() {
+  _start () {
 
-        clearTimeout( this.startTimer );
-        this.running = false;
-    }
+    this.running = true
 
-    _complete() {
+    this.startTime = Date.now()
+    this.currentTime = this.startTime
 
-        this._onComplete();
-        this._destroy();
-    }
+    this.animationFrame = requestAnimationFrame( () => this._tick() )
+  }
 
-    _destroy() {
+  _kill () {
 
-        this.easing = null;
-        this._easingFunction = null;
-        this._onTick = null;
-        this._onComplete = null;
-        this.animationFrame = null;
-        clearTimeout( this.startTimer );
-        this.startTimer = null;
-    }
+    clearTimeout( this.startTimer )
+    this.running = false
+  }
 
-    start(delay = 0) {
+  _complete () {
 
-        clearTimeout( this.startTimer );
-        this.startTimer = setTimeout( () => this._start(), delay );
-    }
+    this._onComplete()
+    this._destroy()
+  }
 
-    kill() {
+  _destroy () {
 
-        this._kill();
-    }
+    this.easing = null
+    this._easingFunction = null
+    this._onTick = null
+    this._onComplete = null
+    this.animationFrame = null
+    clearTimeout( this.startTimer )
+    this.startTimer = null
+  }
+
+  start( delay = 0 ) {
+
+    clearTimeout( this.startTimer )
+    this.startTimer = setTimeout( () => this._start(), delay )
+  }
+
+  kill () {
+
+    this._kill()
+  }
 }
