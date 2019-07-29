977a07b24c8767751b7ce675f1f0781dcf05357f
Kelly Milligan <kelly@milligan.net.nz>
2019-07-29 18:33:18 -0400

    Add scalingFactorOfRotatedRect util

---
+++ rectangle/scaling_factor_of_rotated.js
@@ -0,0 +1,14 @@
+/*
+    Calculate the scaling factor of a rotated rectangle
+    https://math.stackexchange.com/a/438573
+    ---
+    width     Number      Width of rotated rect
+    height    Number      Height of rotated rect
+    theta     Number      Angle of rotation in radians
+    ---
+    Returns   Number      'k', the scaling factor of the outer bounding rect
+*/
+
+export default function scalingFactorOfRotatedRect( width, height, theta ) {
+  return Math.cos( theta ) + ( width / height ) * Math.sin( theta )
+}
