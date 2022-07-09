2cd07ed24353dc1af83ff0721f108deb8d62bbe7
Kelly Milligan <kelly@milligan.net.nz>
2022-07-09 15:11:09 +1200

    Refactor poisson disc sampler

--- poisson-disc.js
+++ poisson-disc.js
@@ -1,32 +1,32 @@
-// Based on Mike Bostock's implementation: https://bl.ocks.org/mbostock/19168c663618b7f07158
+/*
+  Based on Mike Bostock's implementation: https://bl.ocks.org/mbostock/19168c663618b7f07158
+*/
 
+import { polar2d } from './math'
 import { range, value } from './random'
 
-const { PI, ceil, min, max, cos, sin } = Math
+const { PI, ceil, min, max } = Math
 
 export const poissonDisc = (width, height, radius = 10, k = 30) => {
-  const cellSize = radius * Math.SQRT1_2,
-    gridWidth = ceil(width / cellSize),
-    gridHeight = ceil(height / cellSize),
-    grid = new Array(gridWidth * gridHeight),
+  const cell = radius * Math.SQRT1_2,
+    cols = ceil(width / cell),
+    rows = ceil(height / cell),
+    grid = new Array(cols * rows),
     queue = []
-
-  let queueSize = 0,
-    sampleSize = 0
 
   const boundary = (x, y) => x > 0 && x < width && y > 0 && y < height
 
-  const locate = (x, y) => [~~(x / cellSize), ~~(y / cellSize)]
+  const locate = (x, y) => [~~(x / cell), ~~(y / cell)]
 
   const attempt = (x, y) => {
     const location = locate(x, y),
       i0 = max(location[0] - 2, 0),
       j0 = max(location[1] - 2, 0),
-      i1 = min(location[0] + 3, gridWidth),
-      j1 = min(location[1] + 3, gridHeight)
+      i1 = min(location[0] + 3, cols),
+      j1 = min(location[1] + 3, rows)
 
     for (let j = j0; j < j1; ++j) {
-      const col = j * gridWidth
+      const col = j * cols
       for (let i = i0; i < i1; ++i) {
         const cell = grid[col + i]
         if (cell) {
@@ -40,37 +40,36 @@ export const poissonDisc = (width, height, radius = 10, k = 30) => {
   }
 
   const place = (x, y) => {
-    const s = [x, y],
+    const sample = [x, y],
       location = locate(x, y)
-    queue.push(s)
-    grid[gridWidth * location[1] + location[0]] = s
-    sampleSize++
-    queueSize++
-    return s
+    queue.push(sample)
+    grid[cols * location[1] + location[0]] = sample
+    return sample
   }
 
   return () => {
     // Return random sample on first request
-    if (!sampleSize) return place(value() * width, value() * height)
+    if (!queue.length) return place(value() * width, value() * height)
 
     // Pick a random item from the queue to sample against
-    while (queueSize) {
-      const i = ~~(value() * queueSize),
-        s = queue[i]
+    do {
+      const index = ~~(value() * queue.length),
+        origin = queue[index]
 
       // Fetch new placement candidate
       for (let j = 0; j < k; ++j) {
-        const theta = 2 * PI * value(),
-          r = range(radius, 2 * radius),
-          x = s[0] + r * cos(theta),
-          y = s[1] + r * sin(theta)
+        const [x, y] = polar2d(
+          range(radius, 2 * radius),
+          2 * PI * value(),
+          origin
+        )
 
         // Accept if within bounds and valid
         if (boundary(x, y) && attempt(x, y)) return place(x, y)
       }
 
-      queue[i] = queue[--queueSize]
-      queue.length = queueSize
-    }
+      // Remove from the queue
+      queue.splice(index, 1)
+    } while (queue.length)
   }
 }
